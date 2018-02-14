eb88c88642cd6a6ee8403363639744558d805850
Koki Riho <riho.dare@gmail.com>
2018-02-14 15:08:50 +0900

    User Function

--- SoundMixer/SoundMixer/UserViewController.swift
+++ SoundMixer/SoundMixer/UserViewController.swift
@@ -14,10 +14,9 @@ var user:User?
 class UserViewController: UITableViewController{
 
     @IBAction func AddUser(_ sender: Any) {
-
-        user = User()//Userクラスのインスタンス作成
+       user = User()//Userクラスのインスタンス作成
         user?.Id = userNumber
-        user?.Name = "User ID\(user?.Id)"//ここを任意に入力させて任意の名前をつけさせればいい
+        user?.Name = "User ID : "+(user!.Id).description//ここを任意に入力させて任意の名前をつけさせればいい
         //https://joyplot.com/documents/2016/09/04/swift_textfield_uialertcontroller/ これ見たらできそう
         users.append(user!)
         userNumber += 1;
@@ -32,7 +31,7 @@ class UserViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
 
-        
+        cell.textLabel?.text = users[indexPath.row].Name
         return cell
     }
 }
