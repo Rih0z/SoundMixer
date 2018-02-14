bc10c56ad38d6deae6ffa4320d1e6ed70fed2653
Koki Riho <riho.dare@gmail.com>
2018-02-14 17:05:32 +0900

    change global variable to local variable

--- SoundMixer/SoundMixer/UserViewController.swift
+++ SoundMixer/SoundMixer/UserViewController.swift
@@ -7,31 +7,30 @@
 //
 
 import UIKit
-//クラスの中に入れたいけど受け渡しができるのかわからないのでグローバル変数にしています　利穂
-var userNumber:Int = 1
-var users = [User]()
-var user:User?
-class UserViewController: UITableViewController{
 
+class UserViewController: UITableViewController{
+    var userNumber:Int = 1
+    var users = [User]()
+    var user:User?
     @IBAction func AddUser(_ sender: Any) {
-       user = User()//Userクラスのインスタンス作成
-        user?.Id = userNumber
-        user?.Name = "User ID : "+(user!.Id).description//ここを任意に入力させて任意の名前をつけさせればいい
+       self.user = User()//Userクラスのインスタンス作成
+        self.user?.Id = self.userNumber
+        self.user?.Name = "User ID : "+(self.user!.Id).description//ここを任意に入力させて任意の名前をつけさせればいい
         //https://joyplot.com/documents/2016/09/04/swift_textfield_uialertcontroller/ これ見たらできそう
-        users.append(user!)
-        userNumber += 1;
+        self.users.append(self.user!)
+        self.userNumber += 1;
         self.tableView.reloadData()
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int ) -> Int {
-        return users.count
+        return self.users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
 
-        cell.textLabel?.text = users[indexPath.row].Name
+        cell.textLabel?.text = self.users[indexPath.row].Name
         return cell
     }
     
